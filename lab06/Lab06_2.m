9beb7770421dce22afb492ea0b363faa6f8315af
Egor Devyatkin <inkey.cherry@gmail.com>
2024-05-22 21:55:00 +0300

    lab06 - final (egor loh)

--- lab06/Lab06_2.m
+++ lab06/Lab06_2.m
@@ -1,11 +1,11 @@
 k = 1;
 T = 0.5;
 T1 = 0.8;
-W0 = tf(k, [T*T1, T+T1, 1, 0])
+W0 = tf(k, [T*T1, T+T1, 1, 0]);
 
 kp = 1;
 Ta = 1;
 Td = 10;
-Wp = tf([kp*Ta+Td, kp], [Ta, 1])
+Wp = tf([kp*Ta+Td, kp], [Ta, 1]);
 
-%sisotool(W0, Wp);
+sisotool(W0, Wp);
